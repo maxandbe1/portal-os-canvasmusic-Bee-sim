03dc5da6d688fe19066c0609625660771360970e
maxandbe1 <maxandbe1@proton.me>
2026-05-31 13:54:11 -0400

    Create Viewport.jsx

---
+++ src/layout/Viewport.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import DashboardView from "../modules/dashboard/view.jsx";
+import ConsoleView from "../modules/console/view.jsx";
+import CanvasMusicView from "../modules/canvas-music/view.jsx";
+import BeesimView from "../modules/beesim/view.jsx";
+
+export default function Viewport({ active }) {
+  switch (active) {
+    case "dashboard":
+      return <DashboardView />;
+    case "console":
+      return <ConsoleView />;
+    case "canvas-music":
+      return <CanvasMusicView />;
+    case "beesim":
+      return <BeesimView />;
+    default:
+      return <CanvasMusicView />;
+  }
+}
