f2526fe161b9b075e7a7a7923ef9d5d22822be8a
maxandbe1 <maxandbe1@proton.me>
2026-05-31 15:01:34 -0400

    Update Viewport.jsx

--- src/layout/Viewport.jsx
+++ src/layout/Viewport.jsx
@@ -3,6 +3,8 @@ import DashboardView from "../modules/dashboard/view.jsx";
 import ConsoleView from "../modules/console/view.jsx";
 import CanvasMusicView from "../modules/canvas-music/view.jsx";
 import BeesimView from "../modules/beesim/view.jsx";
+import MemoryView from "../modules/memory/view.jsx";
+import SoundView from "../modules/sound/view.jsx";
 
 export default function Viewport({ active }) {
   switch (active) {
